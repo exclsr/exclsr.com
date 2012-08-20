b86073df1fa23a9db60f36187e57b6c41213cac0
Phil <github@exclsr.com>
2012-08-19 19:09:51 -0700

    Header: Add .active to 'li' instead of 'a'
    If you have something like this:
    
    <ul>
        <li><a href="/">link</a></li>
    </ul>
    
    It's cooler to mark the list item
    as 'active' instead of the anchor tag.

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
 	var $body = $("body");
 	
-	var onHeaderLoad = function() {
+	var onHeaderReady = function() {
 		var $navLinks = $("#header a");	
-		var path = window.location.pathname;
-		console.log(path);
+		var path = window.location.pathname;		
 	
 		$navLinks.each(function() {
 			var el = $(this);			
 			if (el.attr('href') === path) {
-				el.addClass("active");
+				el.parent("li").addClass("active");
 			}
 		});
 	};
 
 	$.get("/assets/inc/header/", function(data) {
 		$body.prepend(data);
-		onHeaderLoad();
+		onHeaderReady();
 	});
 
 	$.get("/assets/inc/footer/", function(data) {
